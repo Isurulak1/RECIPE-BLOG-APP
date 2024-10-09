53f939c1364d4953d54ecd9ea40456a98d162126
Isurulak1 <isurulakshan0417@gmail.com>
2024-10-09 16:20:08 +0530

    created navbar section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
     <div className='max-w-screen-2xl mx-auto'>
+      <Header />
     <Outlet />
     </div>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,15 @@ import { createBrowserRouter,RouterProvider} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 import Home from './pages/home/Home.jsx'
+import ErrorPage from './components/ErrorPage.jsx';
+
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage/>,
     children: [
       {
         path: "/",
